c763143d23e18c0251a62708c8cc37075e6a1d6e
Greem4 <i@greem4.ru>
2023-12-21 17:34:01 +0300

    Added Hippodrome, task javarush

---
+++ storeroomJava/compendium/src/main/java/javaRush/hippodrome/stream/Hippodrome.java
@@ -0,0 +1,92 @@
+package javaRush.hippodrome.stream;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Hippodrome {
+    public static Hippodrome game;
+    private static List<Horse> horses;
+
+
+    public static void main(String[] args) {
+        List<Horse> horseList = new ArrayList<>();
+
+        Horse horse1 = new Horse("horse1", 3.0, 0.0);
+        Horse horse2 = new Horse("horse2", 3.0, 0.0);
+        Horse horse3 = new Horse("horse3", 3.0, 0.0);
+
+        horseList.add(horse1);
+        horseList.add(horse2);
+        horseList.add(horse3);
+
+        game = new Hippodrome(horseList);
+
+        game.run();
+        game.getWinner();
+        game.printWinner();
+    }
+
+    void run() {
+        for (int i = 1; i < 101; i++) {
+            move();
+            print();
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    void move() {
+//        for (javaRush.hippodrome.old.Horse horse : horses) {
+//            horse.move();
+//        }
+
+        horses.forEach(Horse::move);
+
+    }
+
+    void print() {
+//        for (javaRush.hippodrome.old.Horse horse : horses) {
+//            horse.print();
+//        }
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println();
+//        }
+
+        horses.forEach(Horse::print);
+        IntStream.range(0, 10).forEach(i -> System.out.println());
+    }
+
+    public Horse getWinner() {
+//        javaRush.hippodrome.old.Horse winner = null;
+//        double maxDistance = 0;
+//        for (javaRush.hippodrome.old.Horse horse : horses) {
+//            if (horse.getDistance() > maxDistance) {
+//                maxDistance = horse.getDistance();
+//                winner = horse;
+//            }
+//        }
+//        return winner;
+
+        return horses.stream()
+                .max(Comparator.comparing(Horse::getDistance))
+                .orElse(null);
+    }
+
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+}
---
+++ storeroomJava/compendium/src/main/java/javaRush/hippodrome/stream/Horse.java
@@ -0,0 +1,52 @@
+package javaRush.hippodrome.stream;
+
+public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public void move() {
+        distance += speed * Math.random();
+    }
+
+    public void print() {
+        String dots = ".".repeat((int) distance);
+        System.out.println(dots + getName());
+
+//        StringBuilder dotc = new StringBuilder();
+//        for (int i = 0; i < (int) distance; i++) {
+//            dotc.append(".");
+//        }
+//        System.out.println(dotc + getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
