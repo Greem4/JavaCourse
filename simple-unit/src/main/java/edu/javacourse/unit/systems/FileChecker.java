db3d0722a3b5432c7e0bf9057122f0155e4ff098
Greem4 <i@greem4.ru>
2024-01-07 21:35:40 +0300

    L 4 unit and added all class for unit test and use ReflectionTestUtils

---
+++ simple-unit/src/main/java/edu/javacourse/unit/systems/FileChecker.java
@@ -0,0 +1,12 @@
+package edu.javacourse.unit.systems;
+
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+@Service
+public class FileChecker
+{
+    public boolean checkFile(InputStream file, String fileName) {
+        return !fileName.endsWith(".exe");
+    }
+}
