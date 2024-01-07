c95143f34f7ff3e667f01d5c2b8cdbb30928d5fa
Greem4 <i@greem4.ru>
2024-01-07 13:14:33 +0300

    r 3 unit and added alfrescoManager

---
+++ simple-unit/src/main/java/edu/javacourse/unit/manager/AlfrescoManager.java
@@ -0,0 +1,9 @@
+package edu.javacourse.unit.manager;
+
+import java.io.InputStream;
+
+public class AlfrescoManager {
+    public String uploadFile(InputStream file, String fileName) {
+        return null;
+    }
+}
--- simple-unit/src/main/java/edu/javacourse/unit/manager/DocumentManager.java
+++ simple-unit/src/main/java/edu/javacourse/unit/manager/DocumentManager.java
@@ -1,7 +1,18 @@
 package edu.javacourse.unit.manager;
 
+import java.io.InputStream;
+
 public class DocumentManager {
-    public void uploadFile() {
-        System.out.println("Called");
+    private AlfrescoManager alfrescoManager;
+    public void uploadFile(InputStream file, String fileName, Long companyID,
+                           boolean external, boolean restricted) {
+        // checkFile
+        // Save to Alfresco
+        alfrescoManager.uploadFile(file, fileName);
+        // if (external) - Save to Amazon S3
+        // if (restricted) - set permissions in Alfresco
+        // Save to DB
     }
+
+
 }
