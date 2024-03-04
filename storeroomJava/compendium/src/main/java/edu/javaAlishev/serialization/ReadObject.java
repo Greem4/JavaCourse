c0cdf6f7299d7be7b776ccc4f4d841331a53aee6
Greem4 <i@greem4.ru>
2024-03-04 18:14:17 +0300

    less Alishev r47 ser

--- storeroomJava/compendium/src/main/java/edu/javaAlishev/serialization/ReadObject.java
+++ storeroomJava/compendium/src/main/java/edu/javaAlishev/serialization/ReadObject.java
@@ -3,17 +3,17 @@ package edu.javaAlishev.serialization;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
         try (FileInputStream fis = new FileInputStream("people.bin");
              ObjectInputStream ois = new ObjectInputStream(fis)) {
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
+            Person[] people = (Person[]) ois.readObject();
 
-            System.out.println(person1);
-            System.out.println(person2);
+            System.out.println(Arrays.toString(people));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
