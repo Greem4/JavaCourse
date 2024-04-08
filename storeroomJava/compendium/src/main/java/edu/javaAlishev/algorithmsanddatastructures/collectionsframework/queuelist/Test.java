d14cb85125fb80b7c110bdde7b1d3fdd1236cf17
Greem4 <i@greem4.ru>
2024-04-08 21:15:06 +0300

    less Alishev  added queue

---
+++ storeroomJava/compendium/src/main/java/edu/javaAlishev/algorithmsanddatastructures/collectionsframework/queuelist/Test.java
@@ -0,0 +1,42 @@
+package edu.javaAlishev.algorithmsanddatastructures.collectionsframework.queuelist;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person1 = new Person(1);
+        Person person2 = new Person(2);
+        Person person3 = new Person(3);
+        Person person4 = new Person(4);
+
+        Queue<Person> people = new ArrayBlockingQueue<Person>(3);
+//        people.add(person3);
+//        people.add(person2);
+//        people.add(person4);
+//        people.add(person1);
+
+        System.out.println(people.offer(person3));
+        System.out.println(people.offer(person2));
+        System.out.println(people.offer(person4));
+        System.out.println(people.offer(person1));
+
+        System.out.println();
+    }
+}
+
+class Person {
+    private int id;
+
+    public Person(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                '}';
+    }
+}
