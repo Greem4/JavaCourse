c5c2f0a745e5b1fb64c9beb6256f85503257025d
Greem4 <i@greem4.ru>
2024-04-29 22:56:32 +0300

    test5

---
+++ storeroomJava/compendium/src/main/java/edu/javaAlishev/algorithmsanddatastructures/collectionsframework/arraylist/Main.java
@@ -0,0 +1,7 @@
+package edu.javaAlishev.algorithmsanddatastructures.collectionsframework.arraylist;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+    }
+}
