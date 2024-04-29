22d5e2b12e4cd45f42808957ac5afeb10e843bf9
Greem4 <134551415+Greem4@users.noreply.github.com>
2024-04-29 22:58:13 +0300

    test commit merge
    * test5-2

---
+++ storeroomJava/compendium/src/main/java/edu/javaAlishev/algorithmsanddatastructures/collectionsframework/arraylist/Main.java
@@ -0,0 +1,11 @@
+package edu.javaAlishev.algorithmsanddatastructures.collectionsframework.arraylist;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+
+
+
+
+    }
+}
