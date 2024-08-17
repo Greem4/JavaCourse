325c6d0a269926f41d83157cdb4465132837701c
Greem4 <i@greem4.ru>
2024-08-17 21:47:12 +0300

    :construction: SQL requests 2

--- SQLdev/date.sql
+++ SQLdev/date.sql
@@ -34,15 +34,18 @@ CREATE TABLE employee
     id         SERIAL PRIMARY KEY,
     first_name varchar(128) NOT NULL,
     last_name  varchar(128) NOT NULL,
+    company_id INT REFERENCES company (id),
     salary     INT,
     UNIQUE (first_name, last_name)
+--     FOREIGN KEY (company_id) REFERENCES company(id)
 );
 
-INSERT INTO employee (first_name, last_name, salary)
-values ('Ivan', 'Sidorov', 500),
-       ('Ivan', 'Ivanov', 1000),
-       ('Petr', 'Petrov', 2000),
-       ('Sveta', 'Svetikova', 1500);
+INSERT INTO employee (first_name, last_name, salary, company_id)
+values ('Ivan', 'Sidorov', 500, 1),
+       ('Ivan', 'Ivanov', 1000, 2),
+       ('Arny', 'Paramonov', null, 2),
+       ('Petr', 'Petrov', 2000, 3),
+       ('Sveta', 'Svetikova', 1500, NULL);
 
 SELECT DISTINCT id,
                 first_name f_name,
@@ -54,7 +57,44 @@ WHERE salary IN (1000, 1100, 2000)
     AND last_name ILIKE '%ov%')
 ORDER BY first_name, salary DESC;
 
+SELECT count(*)
+from employee;
 
+SELECT upper(first_name),
+       now()
+FROM employee empl;
 
+SELECT first_name
+FROM employee
+WHERE company_id IS NOT NULL
+UNION
+SELECT
+    first_name
+FROM employee
+WHERE salary IS NULL;
 
+SELECT
+    avg(emp.salary)
+FROM (SELECT
+          *
+      FROM employee
+      ORDER BY salary
+      LIMIT 2) emp;
 
+SELECT
+    *
+FROM employee
+ORDER BY salary
+LIMIT 2;
+
+select *,
+       (select max(salary) from employee) - salary dif
+from employee;
+
+select *
+from employee
+where company_id IN (select company_id from company where date > '2000-01-01');
+
+SELECT * FROM (SELECT * FROM (VALUES (1, 'Google', '2001-01-01'),
+       (2, 'Apple', '2002-10-29'),
+       (3, 'Facebook', '1998-03-13')) t) y;
