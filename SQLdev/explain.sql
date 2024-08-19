37d39bbbddce7b8c1d1ba4e0a4ccf7764d529b0b
Greem4 <i@greem4.ru>
2024-08-19 22:38:53 +0300

    :construction: SQL add requests 5-1

--- SQLdev/explain.sql
+++ SQLdev/explain.sql
@@ -7,7 +7,12 @@ where passenger_name like 'Иван%'
 EXPLAIN
 SELECT flight_id, count(*), sum(cost)
 FROm ticket
-group by flight_id ;
+group by flight_id;
+
+SELECT avg(bit_length(passenger_no) / 8),
+       avg(bit_length(passenger_name) / 8),
+       avg(bit_length(seat_no) / 8)
+from ticket;
 
 SELECT reltuples,
        relkind,
@@ -15,7 +20,38 @@ SELECT reltuples,
 FROM pg_class
 where relname = 'ticket';
 
-SELECT avg(bit_length(passenger_no) / 8),
-       avg(bit_length(passenger_name) / 8),
-       avg(bit_length(seat_no) / 8)
-from ticket;
+EXPLAIN
+SELECT *
+FROM ticket
+WHERE id = 25;
+
+create table test1
+(
+    id      SERIAL PRIMARY KEY,
+    number1 INT         NOT NULL,
+    number2 INT         NOT NULL,
+    value   VARCHAR(32) NOT NULL
+);
+
+create index test1_number1_idx ON test1 (number1);
+create index test1_number2_idx ON test1 (number2);
+
+insert into test1 (number1, number2, value)
+select random() * generate_series,
+       random() * generate_series,
+       generate_series
+from generate_series(1, 100000);
+
+SELECT relname,
+       reltuples,
+       relkind,
+       relpages
+FROM pg_class
+where relname like 'test1%';
+
+analyse test1;
+
+explain
+select *
+from test1
+where number1 < 1000 and number1 > 90000;
