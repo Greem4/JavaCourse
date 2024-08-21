2d4dbc36c4c27d0b38ed9fba37555d6e263c7c47
Greem4 <i@greem4.ru>
2024-08-21 21:22:21 +0300

    :sparkles: SQL add audit and trigger function

--- SQLdev/explain.sql
+++ SQLdev/explain.sql
@@ -54,4 +54,70 @@ analyse test1;
 explain
 select *
 from test1
-where number1 < 1000 and number1 > 90000;
+where number1 < 1000
+  and number1 > 90000;
+
+EXPLAIN ANALYSE
+SELECT *
+from test1
+where number1 < 1000;
+
+CREATE TABLE test2
+(
+    id       SERIAL PRIMARY KEY,
+    test1_id INT REFERENCES test1 (id) NOT NULL,
+    number1  INT                       NOT NULL,
+    number2  INT                       NOT NULL,
+    value    VARCHAR(32)               NOT NULL
+);
+
+INSERT INTO test2 (test1_id, number1, number2, value)
+SELECT id,
+       random() * number1,
+       random() * number2,
+       value
+FROM test1;
+
+create index test2_number1_idx ON test2 (number1);
+create index test2_number2_idx ON test2 (number2);
+create index test2_test1_id_idx ON test2 (test1_id);
+
+ANALYSE test2;
+
+EXPLAIN ANALYSE
+SELECT *
+FROM test1 t1
+-- JOIN (SELECT * FROM test2 ORDER BY test1_id) t2
+         JOIN test2 t2
+              on t1.id = t2.test1_id;
+
+CREATE TABLE audit
+(
+    id         INT,
+    table_name TEXT,
+    date       TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION audit_function() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+begin
+    insert into audit (id, table_name, date)
+    values (new.id, tg_table_name, now());
+    return null;
+end;
+
+$$;
+
+CREATE TRIGGER audit_aircraft_trigger
+    AFTER UPDATE OR INSERT OR DELETE
+    ON aircraft
+    FOR EACH ROW
+EXECUTE FUNCTION audit_function();
+
+insert into aircraft (model)
+values ('новый боинг');
+
+select *
+from audit;
