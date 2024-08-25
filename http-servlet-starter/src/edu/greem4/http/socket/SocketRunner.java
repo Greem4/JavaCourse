13375371dd11490d5bc6f6cbfdc98cf71dd4ef53
Greem4 <i@greem4.ru>
2024-08-25 16:58:29 +0300

    :technologist: Implement SocketRunner to send user input to localhost on port 7777 and create SocketServerRunner to handle requests and respond to client.

--- http-servlet-starter/src/edu/greem4/http/socket/SocketRunner.java
+++ http-servlet-starter/src/edu/greem4/http/socket/SocketRunner.java
@@ -5,19 +5,23 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
         // http - 80
         // https - 443
         // tcp
-        var inetAddress = Inet4Address.getByName("google.com");
-        try (var socket = new Socket(inetAddress, 80);
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (var socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
-            outputStream.writeUTF("Hello World");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
         }
     }
 }
