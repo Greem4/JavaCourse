2c16e7462e3332a94f06bd0e23cdb43247a7637f
Greem4 <i@greem4.ru>
2024-08-27 18:09:32 +0300

    :sparkles: Add initial implementation of HTTP client and server with JSON configuration and HTML response.

---
+++ http-servlet-starter/src/edu/greem4/http/server/HttpClientRunner.java
@@ -0,0 +1,28 @@
+package edu.greem4.http.server;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+public class HttpClientRunner {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:9000"))
+                .headers("Content-Type", "application/json")
+                .POST(BodyPublishers.ofFile(Path.of("resources", "first.json")))
+                .build();
+
+        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.headers());
+        System.out.println(response.body());
+    }
+}
