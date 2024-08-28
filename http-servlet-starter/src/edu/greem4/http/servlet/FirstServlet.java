07653e757cc250ab693638d16587e78df9c02037
Greem4 <i@greem4.ru>
2024-08-28 16:09:56 +0300

    :sparkles: Refactor FirstServlet to handle GET and POST requests with parameter logging

--- http-servlet-starter/src/edu/greem4/http/servlet/FirstServlet.java
+++ http-servlet-starter/src/edu/greem4/http/servlet/FirstServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -21,17 +22,21 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var headerNames = req.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            var header = headerNames.nextElement();
-            System.out.println(req.getHeader(header));
-        }
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
 
         resp.setContentType("text/html; charset=utf-8");
         resp.setHeader("token", "12345");
         try (var writer = resp.getWriter()) {
             writer.write("<h1>Привет c первого сервлета!</h2>");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
     }
 
     @Override
