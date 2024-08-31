c17ea8b6613a33ade7c975f075475353e0f8fa73
Greem4 <i@greem4.ru>
2024-08-31 15:59:43 +0300

    :technologist: Enhance ContentServlet to retrieve and store flight data, update JSP to display flight information.

--- http-servlet-starter/src/edu/greem4/http/servlet/ContentServlet.java
+++ http-servlet-starter/src/edu/greem4/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package edu.greem4.http.servlet;
 
+import edu.greem4.http.dto.FlightDto;
+import edu.greem4.http.service.FlightService;
 import edu.greem4.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flights", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
