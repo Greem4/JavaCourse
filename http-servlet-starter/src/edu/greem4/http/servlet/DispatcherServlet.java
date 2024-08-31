f25f9b035b3526b3aa1debc74d70ed81a509968f
Greem4 <i@greem4.ru>
2024-08-31 13:49:43 +0300

    :bento: Implement initial DispatcherServlet with GET handling and request forwarding functionality.

---
+++ http-servlet-starter/src/edu/greem4/http/servlet/DispatcherServlet.java
@@ -0,0 +1,23 @@
+package edu.greem4.http.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/dispatcher")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/flights")
+                .forward(req, resp);
+
+        System.out.println();
+//        req.setAttribute("1", "234");
+    }
+}
