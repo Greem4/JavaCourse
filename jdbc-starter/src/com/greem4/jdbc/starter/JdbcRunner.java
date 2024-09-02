191127d6fbd2bd79ff250cbfad925f627c840280
Greem4 <i@greem4.ru>
2024-09-02 21:06:01 +0300

    :construction: Add SQL table creation to JdbcRunner and configure IntelliJ code style
    - Introduce SQL statement to create 'info' table in JdbcRunner.
    - Update connection management to streamline statement execution.
    - Add IntelliJ code style configuration file for consistent formatting.

--- jdbc-starter/src/com/greem4/jdbc/starter/JdbcRunner.java
+++ jdbc-starter/src/com/greem4/jdbc/starter/JdbcRunner.java
@@ -8,10 +8,20 @@ import java.sql.SQLException;
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
-        try (var connection = ConnectionManager.open()) {
-            try (var statement = connection.createStatement()) {
-                System.out.println(connection.getTransactionIsolation());
-            }
+        String sql = """
+                                CREATE TABLE IF NOT EXISTS info (
+                        id SERIAL PRIMARY KEY,
+                        data TEXT NOT NULL
+                );
+                                """;
+
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getTransactionIsolation());
+            System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
+
