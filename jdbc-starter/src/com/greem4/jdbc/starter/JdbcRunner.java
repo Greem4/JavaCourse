f0508309541239e056fe886ea256a6ca60d3162d
Greem4 <i@greem4.ru>
2024-09-02 19:42:31 +0300

    :construction: Add project configuration files and implement database connection management
    - Create IDE configuration files for project setup.
    - Implement `ConnectionManager` class to manage PostgreSQL database connections.
    - Update `JdbcRunner` class to utilize `ConnectionManager` for opening database connections and displaying transaction isolation level.

--- jdbc-starter/src/com/greem4/jdbc/starter/JdbcRunner.java
+++ jdbc-starter/src/com/greem4/jdbc/starter/JdbcRunner.java
@@ -1,8 +1,17 @@
 package com.greem4.jdbc.starter;
 
+import com.greem4.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
+import java.sql.SQLException;
+
 public class JdbcRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        try (var connection = ConnectionManager.open()) {
+            try (var statement = connection.createStatement()) {
+                System.out.println(connection.getTransactionIsolation());
+            }
+        }
     }
 }
