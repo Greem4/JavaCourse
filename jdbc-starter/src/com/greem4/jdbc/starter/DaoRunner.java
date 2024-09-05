e38541969abed1771b21532734920c131806ac98
Greem4 <i@greem4.ru>
2024-09-05 22:27:37 +0300

    :sparkles: Implement ticket management features in DaoRunner and TicketDao
    - Add methods to find all tickets and find a ticket by ID in TicketDao.
    - Implement the update functionality for tickets within TicketDao.
    - Create test methods in DaoRunner to demonstrate ticket updates and fetching all tickets.
    - Enhance SQL queries with proper structure for fetching and updating tickets.

--- jdbc-starter/src/com/greem4/jdbc/starter/DaoRunner.java
+++ jdbc-starter/src/com/greem4/jdbc/starter/DaoRunner.java
@@ -4,10 +4,27 @@ import com.greem4.jdbc.starter.dao.TicketDao;
 import com.greem4.jdbc.starter.entity.Ticket;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class DaoRunner {
 
     public static void main(String[] args) {
+        var tickets = TicketDao.getInstance().findAll();
+        System.out.println();
+    }
+
+    private static void updateTest() {
+        var ticketDao = TicketDao.getInstance();
+        var maybeTicket = ticketDao.findById(2L);
+        System.out.println(maybeTicket);
+
+        maybeTicket.ifPresent(ticket -> {
+            ticket.setCost(BigDecimal.valueOf(188.88));
+            ticketDao.update(ticket);
+        });
+    }
+
+    private static void deleteTest() {
         var ticketDao = TicketDao.getInstance();
         ticketDao.delete(56L);
     }
--- jdbc-starter/src/com/greem4/jdbc/starter/dao/TicketDao.java
+++ jdbc-starter/src/com/greem4/jdbc/starter/dao/TicketDao.java
@@ -4,7 +4,10 @@ import com.greem4.jdbc.starter.entity.Ticket;
 import com.greem4.jdbc.starter.util.ConnectionManager;
 import lombok.SneakyThrows;
 
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class TicketDao {
 
@@ -18,13 +21,85 @@ public class TicketDao {
             INSERT INTO ticket (passenger_no, passenger_name, flight_id, seat_no, cost)
             VALUES (?,?,?,?,?);
             """;
+    private static final String UPDATE_SQL = """
+            UPDATE ticket
+            SET passenger_no = ?,
+                passenger_name = ?,
+                flight_id = ?,
+                seat_no = ?,
+                cost = ?
+            WHERE id = ?
+            """;
+    private static final String FIND_ALL_SQL = """
+            SELECT id, passenger_no, passenger_name, flight_id, seat_no, cost
+            FROM ticket
+            """;
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + """
+            WHERE id = ?
+            """;
 
-    private TicketDao() {}
+    private TicketDao() {
+    }
+
+    @SneakyThrows
+    public List<Ticket> findAll() {
+        try (var connection = ConnectionManager.get();
+             var preparedStatement = connection.prepareStatement(FIND_ALL_SQL)) {
+            var resultSet = preparedStatement.executeQuery();
+            List<Ticket> tickets = new ArrayList<>();
+            while (resultSet.next()) {
+                tickets.add(buildTicket(resultSet));
+            }
+            return tickets;
+        }
+    }
+
+    @SneakyThrows
+    public Optional<Ticket> findById(Long id) {
+        try (var connection = ConnectionManager.get();
+             var preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            preparedStatement.setLong(1, id);
+
+            var resultSet = preparedStatement.executeQuery();
+            Ticket ticket = null;
+            if (resultSet.next()) {
+                ticket = buildTicket(resultSet);
+            }
+
+            return Optional.ofNullable(ticket);
+        }
+    }
+
+    private static Ticket buildTicket(ResultSet resultSet) throws SQLException {
+        return new Ticket(
+                resultSet.getLong("id"),
+                resultSet.getString("passenger_no"),
+                resultSet.getString("passenger_name"),
+                resultSet.getLong("flight_id"),
+                resultSet.getString("seat_no"),
+                resultSet.getBigDecimal("cost")
+        );
+    }
+
+    @SneakyThrows
+    public void update(Ticket ticket) {
+        try (var connection = ConnectionManager.get();
+        var preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+            preparedStatement.setString(1, ticket.getPassengerNo());
+            preparedStatement.setString(2, ticket.getPassengerName());
+            preparedStatement.setLong(3, ticket.getFlightId());
+            preparedStatement.setString(4, ticket.getSeatNo());
+            preparedStatement.setBigDecimal(5, ticket.getCost());
+            preparedStatement.setLong(6, ticket.getId());
+
+            preparedStatement.executeUpdate();
+        }
+    }
 
     @SneakyThrows
     public Ticket save(Ticket ticket) {
-        try (var connection = ConnectionManager.get()) {
-            var preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS);
+        try (var connection = ConnectionManager.get();
+             var preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, ticket.getPassengerNo());
             preparedStatement.setString(2, ticket.getPassengerName());
             preparedStatement.setLong(3, ticket.getFlightId());
@@ -43,8 +118,8 @@ public class TicketDao {
 
     @SneakyThrows
     public boolean delete(Long id) {
-        try (var connection = ConnectionManager.get()) {
-            var preparedStatement = connection.prepareStatement(DELETE_SQL);
+        try (var connection = ConnectionManager.get();
+             var preparedStatement = connection.prepareStatement(DELETE_SQL)) {
             preparedStatement.setLong(1, id);
 
             return preparedStatement.executeUpdate() > 0;
