e23aeb9a3d7d48f3ecd2ef9dcb7124a282c46a53
Greem4 <i@greem4.ru>
2024-09-09 23:58:51 +0300

    :sparkles: Implement user entity with Hibernate mappings and session operations
    - Add `User` entity class with fields for username, firstname, lastName, birthday, and age.
    - Configure Hibernate to map the `User` class to the "users" table in the public schema.
    - Update `HibernateRunner` to save a new user instance to the database.
    - Enable SQL logging in Hibernate configuration for easier debugging.

--- hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
+++ hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
@@ -1,21 +1,36 @@
 package com.greem4;
 
+import com.greem4.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
 
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastName("Ivanov")
+                    .birthday(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
