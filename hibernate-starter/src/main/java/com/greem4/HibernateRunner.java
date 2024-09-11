db889d3244432804682c55e4b5d888c2ba4ffea1
Greem4 <i@greem4.ru>
2024-09-11 21:07:28 +0300

    :zap: Add SLF4J and Log4j for logging in HibernateRunner
    - Integrate SLF4J and Log4j dependencies to enhance logging capabilities.
    - Include log statements to trace user entity states and transaction lifecycle.
    - Create a Log4j configuration file for structured logging output.

--- hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
+++ hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
@@ -4,11 +4,16 @@ import com.greem4.entity.User;
 import com.greem4.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 
 public class HibernateRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
@@ -16,25 +21,22 @@ public class HibernateRunner {
                 .lastname("Ivanov")
                 .firstname("Ivan")
                 .build();
+        log.info("User entity is in transient state, object: {}", user);
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.trace("User is in persistent state: {} session1: {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setFirstname("Sveta");
-//                session2.delete(user);
-//                session2.refresh(user);
-                session2.merge(user);
-
-                session2.getTransaction().commit();
-            }
+            log.warn("User is im detached state: {}, session is closed {}", user, session1);
+        }catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
+            throw exception;
         }
     }
 }
