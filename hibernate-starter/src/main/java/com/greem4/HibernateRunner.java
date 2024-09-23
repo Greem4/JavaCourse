8fb31d6b703953c918798a0cded68e1268d6fcd8
Greem4 <i@greem4.ru>
2024-09-23 21:54:27 +0300

    :construction: 67

--- hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
+++ hibernate-starter/src/main/java/com/greem4/HibernateRunner.java
@@ -1,6 +1,8 @@
 package com.greem4;
 
 import com.greem4.entity.Payment;
+import com.greem4.entity.Profile;
+import com.greem4.entity.User;
 import com.greem4.util.HibernateUtil;
 import com.greem4.util.TestDataImporter;
 import lombok.extern.slf4j.Slf4j;
@@ -24,22 +26,27 @@ public class HibernateRunner {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
             TestDataImporter.importData(sessionFactory);
+//            session.doWork(connection -> connection.setAutoCommit(false));
 
-//            session.setDefaultReadOnly(true);
-            session.beginTransaction();
+//            session.beginTransaction();
 
-            session.createNativeQuery("SET TRANSACTION READ ONLY ").executeUpdate();
+            var profile = Profile.builder()
+                    .user(session.find(User.class, 1L))
+                    .language("ru")
+                    .street("Kolasa 28")
+                    .build();
+            session.save(profile);
 
-//            var payments = session.createQuery("select p from Payment p", Payment.class)
+            var payments = session.createQuery("select p from Payment p", Payment.class)
 //                    .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
 //                    .setHint("javax.persistence.lock.timeout", 5000)
 //                    .setReadOnly(true)
-//                    .list();
+                    .list();
 
             var payment = session.find(Payment.class, 1L);
             payment.setAmount(payment.getAmount() + 10);
 
-            session.getTransaction().commit();
+//            session.getTransaction().commit();
         }
     }
 }
