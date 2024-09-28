f43b7f9fde34b62c3ee5ace6763e3b4cea38996a
Greem4 <i@greem4.ru>
2024-09-28 19:28:56 +0300

    :construction: 5 Add Spring framework dependencies and update ApplicationRunner
    - Include `spring-core` and `spring-context` dependencies in the build file.
    - Refactor `ApplicationRunner` to use `ClassPathXmlApplicationContext`.
    - Add XML configuration for `ConnectionPool` beans in a new XML file.

--- spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
+++ spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
@@ -1,26 +1,13 @@
 package com.greem4.spring;
 
 import com.greem4.spring.database.pool.ConnectionPool;
-import com.greem4.spring.database.repository.CompanyRepository;
-import com.greem4.spring.database.repository.UserRepository;
-import com.greem4.spring.ioc.Container;
-import com.greem4.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
-
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository =  container.get(UserRepository.class);
-//        var companyRepository =  container.get(CompanyRepository.class);
-        var userService =  container.get(UserService.class);
-        // TODO: 28.09.2024 userService
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
