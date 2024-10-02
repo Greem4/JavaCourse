a51bff0c282b090c373f2014d9ff01e46b4b72f5
Greem4 <i@greem4.ru>
2024-10-02 20:41:54 +0300

    19 less Add Spring component annotations and refactor ConnectionPool initialization
    - Annotate `ConnectionPool`, `VerifyPropertyBeanFactoryPostProcessor`, `LogBeanFactoryPostProcessor`, `TransactionBeanPostProcessor`, `InjectBeanPostProcessor`, and `AuditingBeanPostProcessor` with `@Component` for Spring management.
    - Use constructor injection in `CompanyRepository` and `UserService` for `ConnectionPool` and repositories.
    - Modify `ApplicationRunner` to retrieve `ConnectionPool` bean from the application context.
    - Update XML configuration to enable component scanning and property placeholder for `application.properties`.

--- spring-starter/src/main/java/com/greem4/spring/database/repository/UserRepository.java
+++ spring-starter/src/main/java/com/greem4/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.greem4.spring.database.repository;
 
 import com.greem4.spring.database.pool.ConnectionPool;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepository {
 
     private final ConnectionPool connectionPool;
--- spring-starter/src/main/java/com/greem4/spring/service/UserService.java
+++ spring-starter/src/main/java/com/greem4/spring/service/UserService.java
@@ -1,20 +1,20 @@
 package com.greem4.spring.service;
 
+import com.greem4.spring.database.entity.Company;
 import com.greem4.spring.database.repository.CompanyRepository;
+import com.greem4.spring.database.repository.CrudRepository;
 import com.greem4.spring.database.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final CompanyRepository companyRepository;
-    private CompanyService companyService;
+    private final CrudRepository<Integer, Company> companyRepository;
 
-    public UserService(UserRepository userRepository, CompanyRepository companyRepository) {
+    public UserService(UserRepository userRepository,
+                       CrudRepository<Integer, Company> companyRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
-    }
-
-    public void setCompanyService(CompanyService companyService) {
-        this.companyService = companyService;
     }
 }
