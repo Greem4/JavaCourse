137bc082b16c9dc02c014b6301cccf2b1a4b2e97
Greem4 <i@greem4.ru>
2024-10-02 17:00:29 +0300

    16-15 less Enhance dependency injection with InjectBean annotation and InjectBeanPostProcessor class. Implement application context awareness and field injection for beans. Add InjectBean to CompanyRepository for connection pool management.

--- spring-starter/src/main/java/com/greem4/spring/database/repository/CompanyRepository.java
+++ spring-starter/src/main/java/com/greem4/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.greem4.spring.database.repository;
 
+import com.greem4.spring.bpp.InjectBean;
 import com.greem4.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
