fc84d2e806ec29844251cb16993e4fc34264544d
Greem4 <i@greem4.ru>
2024-10-02 13:25:17 +0300

    12 less Add checks for class type assignments in ApplicationRunner

--- spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
+++ spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
@@ -2,11 +2,19 @@ package com.greem4.spring;
 
 import com.greem4.spring.database.pool.ConnectionPool;
 import com.greem4.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("p1", ConnectionPool.class);
             System.out.println(connectionPool);
