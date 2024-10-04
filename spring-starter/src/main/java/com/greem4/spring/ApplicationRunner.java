d6d113a6de85973d7c0f3951529e7f71794e7bcf
Greem4 <i@greem4.ru>
2024-10-04 14:21:21 +0300

    34 less Add Spring Boot and PostgreSQL dependencies, refactor ApplicationRunner
    - Include Spring Boot plugin and dependency management in build configuration.
    - Add Spring Boot starter and PostgreSQL driver to project dependencies.
    - Refactor ApplicationRunner class to use Spring Boot's application context.

--- spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
+++ spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
@@ -1,31 +1,15 @@
 package com.greem4.spring;
 
-import com.greem4.spring.config.ApplicationConfiguration;
-import com.greem4.spring.database.pool.ConnectionPool;
-import com.greem4.spring.service.CompanyService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import java.io.Serializable;
-
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+        var context = SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println(context.getBeanDefinitionCount());
 
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
     }
 }
