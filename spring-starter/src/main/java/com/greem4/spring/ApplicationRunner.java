3692b0e5c53bf24eb6ddde33a438566d94a44c06
Greem4 <i@greem4.ru>
2024-10-04 22:20:45 +0300

    36-38 less Add configuration for database properties and update application setup
    - Create `DatabaseProperties` class with fields for database configuration.
    - Annotate `DatabaseProperties` with `@ConfigurationProperties`.
    - Integrate the new class into `ApplicationRunner`.
    - Update property definitions in `application.properties`.
    - Change property format from key-value to YAML structure in `application.yml`.
    - Define placeholder values in `application.yml` for better configurability.

--- spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
+++ spring-starter/src/main/java/com/greem4/spring/ApplicationRunner.java
@@ -1,14 +1,17 @@
 package com.greem4.spring;
 
+import com.greem4.spring.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
-
+        System.out.println(DatabaseProperties.class);
     }
 }
