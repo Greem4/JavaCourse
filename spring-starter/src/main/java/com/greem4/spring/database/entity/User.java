cfa08315145c9bb5eed4afe14e7db0273d93f9f6
Greem4 <i@greem4.ru>
2024-10-23 22:36:36 +0300

    66 less Add Revision entity and enable auditing with Spring Data Envers
    - Create a new `Revision` class to track changes in the database.
    - Update `AuditConfiguration` to enable Envers repositories.
    - Modify `User` entity to include auditing annotations and adjust relationships.
    - Update `UserRepository` to extend `RevisionRepository` for history tracking.
    - Enhance unit tests with auditing checks and configure database properties.

--- spring-starter/src/main/java/com/greem4/spring/database/entity/User.java
+++ spring-starter/src/main/java/com/greem4/spring/database/entity/User.java
@@ -1,6 +1,9 @@
 package com.greem4.spring.database.entity;
 
 import lombok.*;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -18,6 +21,7 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "users")
+@Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
 public class User extends AuditingEntity<Long> {
 
     @Id
@@ -40,6 +44,7 @@ public class User extends AuditingEntity<Long> {
     @JoinColumn(name = "company_id")
     private Company company;
 
+    @NotAudited
     @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<UserChat> userChats = new ArrayList<>();
--- spring-starter/src/main/java/com/greem4/spring/database/repository/UserRepository.java
+++ spring-starter/src/main/java/com/greem4/spring/database/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
@@ -16,7 +17,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> , FilterUserRepository {
+public interface UserRepository extends
+        JpaRepository<User, Long>,
+        FilterUserRepository,
+        RevisionRepository<User, Long, Integer> {
 
     @Query("select u from User u " + "where u.firstname like %:firstname% and u.lastname like %:lastname%")
     List<User> findAllBy(String firstname, String lastname);
@@ -30,7 +34,7 @@ public interface UserRepository extends JpaRepository<User, Long> , FilterUserRe
 
     Optional<User> findTopByOrderByIdDesc();
 
-    @QueryHints(@QueryHint(name = "org.hibernate.fitchSize",value = "50"))
+    @QueryHints(@QueryHint(name = "org.hibernate.fitchSize", value = "50"))
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
 
@@ -45,10 +49,10 @@ public interface UserRepository extends JpaRepository<User, Long> , FilterUserRe
 //    List<PersonalInfo> findAllByCompanyId(Integer CompanyId);
 
     @Query(value = "SELECT firstname, " +
-                   "lastname, " +
-                   "birth_date birthDate " +
-                   "FROM users " +
-                   "WHERE company_id = :companyId",
+            "lastname, " +
+            "birth_date birthDate " +
+            "FROM users " +
+            "WHERE company_id = :companyId",
             nativeQuery = true)
     List<PersonalInfo2> findAllByCompanyId(Integer companyId);
 
