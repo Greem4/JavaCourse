863182f80424e5d78bf5abf8d486e7a9bf8f1fbf
Greem4 <i@greem4.ru>
2024-10-24 21:28:19 +0300

    68 less Implement query method to find users by company ID and role
    Add a new method `findAllByCompanyIdAndRole` in `FilterUserRepository` interface and its implementation in `FilterUserRepositoryImpl` to retrieve user information based on company ID and role. Include a SQL query to fetch the user's firstname, lastname, and birth date. Update tests to verify the new functionality.

--- spring-starter/src/main/java/com/greem4/spring/database/repository/FilterUserRepository.java
+++ spring-starter/src/main/java/com/greem4/spring/database/repository/FilterUserRepository.java
@@ -1,6 +1,8 @@
 package com.greem4.spring.database.repository;
 
+import com.greem4.spring.database.entity.Role;
 import com.greem4.spring.database.entity.User;
+import com.greem4.spring.dto.PersonalInfo;
 import com.greem4.spring.dto.UserFilter;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface FilterUserRepository {
 
     List<User> findAllByFilter(UserFilter filter);
+
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
 }
