198cbbedbc0be5db579e656311054271b392645a
Greem4 <i@greem4.ru>
2024-10-28 20:34:25 +0300

    80 less Add login functionality with DTO and HTML form
    - Create `LoginDto` class for capturing login data.
    - Implement `LoginController` for handling login requests.
    - Add GET and POST mapping methods for login page and submission.
    - Introduce HTML form for user login.
    - Update `GreetingController` to include new model attributes and enhance response handling.

--- spring-starter/src/main/java/com/greem4/spring/http/controller/GreetingController.java
+++ spring-starter/src/main/java/com/greem4/spring/http/controller/GreetingController.java
@@ -1,45 +1,52 @@
 package com.greem4.spring.http.controller;
 
+import com.greem4.spring.database.entity.Role;
 import com.greem4.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
 @SessionAttributes({"user"})
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
-        modelAndView.setViewName("greeting/hello");
-        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+    @ModelAttribute("roles")
+    public List<Role> roles() {
+        return Arrays.asList(Role.values());
+    }
 
-        return modelAndView;
+    @GetMapping("/hello")
+    public String hello(Model modelAndView,
+                        HttpServletRequest request,
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto) {
+        modelAndView.addAttribute("user", new UserReadDto(1L, "Ivan"));
+
+        return "greeting/hello";
+    }
+
+    @GetMapping("/bye")
+    public String bye(@SessionAttribute("user") UserReadDto user, Model model) {
+        return "greeting/bye";
     }
 
     @GetMapping("/hello/{id}")
     public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
-                              @RequestParam Integer age,
-                              @RequestHeader String accept,
-                              @CookieValue("JSESSIONID") String JSESSIONID,
-                              @PathVariable Integer id) {
+                               @RequestParam Integer age,
+                               @RequestHeader String accept,
+                               @CookieValue("JSESSIONID") String JSESSIONID,
+                               @PathVariable Integer id) {
         String ageParamValue = request.getParameter("age");
         String acceptHeader = request.getHeader("accept");
         Cookie[] cookies = request.getCookies();
 
         modelAndView.setViewName("greeting/hello");
-
-        return modelAndView;
-    }
-
-    @GetMapping("/bye")
-    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
 
         return modelAndView;
     }
