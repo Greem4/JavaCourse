0cd1b30ef447f6eef8840d4389ae20756635b77d
Greem4 <i@greem4.ru>
2024-10-29 23:05:36 +0300

    84 less Add `@FieldNameConstants` annotation to `UserCreateEditDto`, update user routes, and implement user controller tests
    - Introduce `@FieldNameConstants` for better field management in `UserCreateEditDto`.
    - Update endpoints in `UserController` for creating, updating, and deleting users.
    - Add integration tests for user retrieval and creation functionalities.
    - Specify `hbm2ddl.auto` as `validate` and set `open-in-view` to `false` in application properties.

--- spring-starter/src/main/java/com/greem4/spring/dto/UserCreateEditDto.java
+++ spring-starter/src/main/java/com/greem4/spring/dto/UserCreateEditDto.java
@@ -2,10 +2,12 @@ package com.greem4.spring.dto;
 
 import com.greem4.spring.database.entity.Role;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
     String username;
     LocalDate birthDate;
--- spring-starter/src/main/java/com/greem4/spring/http/controller/UserController.java
+++ spring-starter/src/main/java/com/greem4/spring/http/controller/UserController.java
@@ -18,12 +18,13 @@ public class UserController {
 
     @GetMapping
     public String findAll(Model model) {
+//        model.addAttribute("users", userService.findAll(filter));
         model.addAttribute("users", userService.findAll());
         return "user/users";
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model) {
+    public String findById(@PathVariable("id") Long id, Model model) {
         return userService.findById(id)
                 .map(user -> {
                     model.addAttribute("user", user);
@@ -33,22 +34,21 @@ public class UserController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String create(UserCreateEditDto user) {
-        userService.create(user);
+//    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@ModelAttribute UserCreateEditDto user) {
         return "redirect:/users/" + userService.create(user).getId();
     }
 
     //    @PutMapping("/{id}")
-    @PutMapping("/{id}/update")
-    public String update(@PathVariable("id") Long id, UserCreateEditDto user) {
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable("id") Long id, @ModelAttribute UserCreateEditDto user) {
         return userService.update(id, user)
                 .map(it -> "redirect:/users/{id}")
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     //    @DeleteMapping("/{id}")
-    @PutMapping("/{id}/delete")
+    @PostMapping("/{id}/delete")
     public String delete(@PathVariable("id") Long id) {
         if (!userService.delete(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
