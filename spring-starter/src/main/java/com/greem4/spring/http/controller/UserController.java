dbf7279199976adc7392ed27d5fc1d7fe1b98662
Greem4 <i@greem4.ru>
2024-11-03 19:48:21 +0300

    93 less Add exception handler and error page for server errors
    - Implement `ControllerExceptionHandler` to log exceptions and return a 500 error page.
    - Create an HTML template for displaying an error message on server errors.
    - Update import statements and use `@Slf4j` for logging throughout the handler.

--- spring-starter/src/main/java/com/greem4/spring/http/controller/UserController.java
+++ spring-starter/src/main/java/com/greem4/spring/http/controller/UserController.java
@@ -10,6 +10,7 @@ import com.greem4.spring.service.UserService;
 import com.greem4.spring.validation.group.CreateAction;
 import com.greem4.spring.validation.group.UpdateAction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -17,16 +18,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.groups.Default;
 
+@Slf4j
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
