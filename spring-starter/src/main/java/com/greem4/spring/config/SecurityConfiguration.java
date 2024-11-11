3fe41a17be1dcf86a8ccdcfcb548844119a51759
Greem4 <i@greem4.ru>
2024-11-11 16:24:13 +0300

    113 less Add Spring Boot Actuator and implement security features
    - Include `spring-boot-starter-actuator` dependency for monitoring and management.
    - Create `PasswordEncoderConfiguration` class to define a `PasswordEncoder` bean.
    - Update `SecurityConfiguration` to enable method security and customize OAuth2 user services.
    - Permit access to additional endpoints and enhance OAuth2 user handling.

--- spring-starter/src/main/java/com/greem4/spring/config/SecurityConfiguration.java
+++ spring-starter/src/main/java/com/greem4/spring/config/SecurityConfiguration.java
@@ -1,16 +1,34 @@
 package com.greem4.spring.config;
 
+import com.greem4.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import static com.greem4.spring.database.entity.Role.*;
 
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests(urlConfig -> urlConfig
                         .antMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .antMatchers("/users/{\\d+}/delete").hasAuthority(ADMIN.getAuthority())
-                        .antMatchers("/admin/**").hasAnyRole(ADMIN.getAuthority())
+                        .antMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
                         .anyRequest().authenticated()
                 )
 //                .httpBasic(Customizer.withDefaults());
@@ -31,12 +49,45 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .loginPage("/login")
                         .defaultSuccessUrl("/users"))
                 .oauth2Login(config -> config
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/users"));
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/users")
+                                .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService()))
+//                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService()))
+                );
+
+    }
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email");
+            UserDetails userDetails = userService.loadUserByUsername(email);
+
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+            Set<Method> userDetailMethods = Set.of(UserDetails.class.getMethods());
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class},
+                    (proxy, method, args) -> userDetailMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args));
+        };
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        return userRequest -> {
+            OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+            String email = (String) oAuth2User.getAttributes().get("default_email");
+            UserDetails userDetails = userService.loadUserByUsername(email);
+
+            Set<Method> userDetailMethods = Set.of(UserDetails.class.getMethods());
+            return (OAuth2User) Proxy.newProxyInstance(
+                    SecurityConfig.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OAuth2User.class},
+                    (proxy, method, args) -> userDetailMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oAuth2User, args)
+            );
+        };
     }
 }
