41729fbcdf40ac407f648396b4b4bf861b279e6f
Greem4 <i@greem4.ru>
2024-11-11 20:13:57 +0300

    114 less Add OpenAPI configuration for OAuth2 authentication
    Implement OpenAPI configuration in `OpenApiConfiguration` class using OAuth2 security scheme with authorization code flow. Set up necessary imports and define URLs for authorization and token retrieval. Update application properties to include client ID and secret for Yandex OAuth integration.

---
+++ spring-starter/src/main/java/com/greem4/spring/config/OpenApiConfiguration.java
@@ -0,0 +1,22 @@
+package com.greem4.spring.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/oauth2/authorization/yandex",
+                        tokenUrl = "https://oauth.yandex.ru/token"
+                )
+        )
+)
+public class OpenApiConfiguration {
+
+}
